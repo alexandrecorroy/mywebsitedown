b225baa0c41a6b4e200de20957b3d0ef8631149b
unknown <alexandre.corroy@galaxiemedia.fr>
2025-01-06 21:16:49 +0100

    fix old code

--- src/Mailer/SendMailOnCronWebLinkScheduleError.php
+++ src/Mailer/SendMailOnCronWebLinkScheduleError.php
@@ -4,21 +4,26 @@ namespace App\Mailer;
 
 use App\Entity\WebLink;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 
 final class SendMailOnCronWebLinkScheduleError
 {
+    private string $email_admin;
 
     public function __construct(
-        private MailerInterface $mailer
+        private MailerInterface $mailer,
+        #[Autowire('%email_admin%')] string $email_admin
     )
-    { }
+    {
+        $this->email_admin = $email_admin;
+    }
 
     public function sendMail(WebLink $webLink)
     {
         $email = (new TemplatedEmail())
-            ->from('andi@galaxiemedia.fr')
+            ->from($this->email_admin)
             ->to(new Address($webLink->getWebLinkSchedule()->getUser()->getEmail()))
             ->subject('New Alert for '.$webLink->getWebLinkSchedule()->getName())
 
